94e79a6774d1d3d0799c85abe45a6e8bebb3589c
M4CTAVICH <ouaretchakib@gmail.com>
2025-06-29 07:47:49 +0100

    fixed typo

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const DB_FILE = process.env.DB_FILE || "clinic.sqlite";
+const DB_FILE = process.env.DB_FILE;
 const dbPath = path.resolve(__dirname, DB_FILE);
 
 const db = new sqlite3.Database(dbPath, (err) => {
